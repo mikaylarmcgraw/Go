a898a3d8e07c2f6ae26968dd6b1995c813e39125
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-12-03 14:32:10 -0500

    adding solution to calculate inital points

--- AdventOfCode2022/Day2/main.go
+++ AdventOfCode2022/Day2/main.go
@@ -16,12 +16,13 @@ type Round struct {
 
 func main() {
 	//create list of rounds
-	//var roundList []Round
+	var roundList []Round
 	//retrieve input and parse into Round structures list with appropriate player selections
-	readInputValues()
+	roundList = readInputValues()
 	//calculate points based on your player selection
-
+	roundList = calculateChoicePoints(roundList)
 	//calculate outcome of game and adjust points accordingly
+
 }
 
 func readInputValues() []Round {
@@ -38,18 +39,39 @@ func readInputValues() []Round {
 
 	//scanning file for next line
 	for scanner.Scan() {
+		//get line of file and save into string
 		input := scanner.Text()
+		//split values of string by space and hold values in array
 		inputArray := strings.Split(input, " ")
+		//obtain individual values from array
 		v1 := inputArray[0]
 		v2 := inputArray[1]
-		fmt.Println("Opponent value: ", v1, " My Choice: ", v2)
-		round := Round{v1, "", "", 0}
+		round := Round{v1, v2, "", 0}
 		list = append(list, round)
 	}
 
 	//error handling on scanner
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
+	}
+
+	return list
+}
+
+func calculateChoicePoints(list []Round) []Round {
+
+	for i, v := range list {
+		switch v.myChoice {
+		// if value is rock add 1 point
+		case "X":
+			list[i].points = 1
+		//if value is paper add 2 points
+		case "Y":
+			list[i].points = 2
+		//if value is scissors add 3 points
+		case "Z":
+			list[i].points = 3
+		}
 	}
 
 	return list
