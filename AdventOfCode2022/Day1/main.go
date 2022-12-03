1d3b3b43487764df184cbd0c52822f737c55805a
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-12-02 23:11:19 -0500

    Adding solution for part 2

--- AdventOfCode2022/Day1/main.go
+++ AdventOfCode2022/Day1/main.go
@@ -23,8 +23,12 @@ func main() {
 	//calculate elf with the most calories
 	var highestCalorieElf = findMostCalorieElf(elfList)
 
-	//print output
+	//print output of highest calorie elf
 	fmt.Println("Highest Calorie Elf is: ", highestCalorieElf.id, " Total calories is: ", highestCalorieElf.calorieCount)
+
+	//print output for part 2 solution of top 3 elves and total calories
+	var topElvesList = findTopElves(elfList, 3)
+	calculateTotalTopElves(topElvesList)
 }
 
 func readFileToGenerateList(elfList []Elf) []Elf {
@@ -64,6 +68,18 @@ func readFileToGenerateList(elfList []Elf) []Elf {
 	return elfList
 }
 
+func findElfIndex(list []Elf, elf Elf) int {
+
+	var index = -1
+
+	for i, v := range list {
+		if elf == v {
+			index = i
+		}
+	}
+	return index
+}
+
 func findMostCalorieElf(list []Elf) Elf {
 	var mostCalorieElf = list[0]
 
@@ -75,4 +91,40 @@ func findMostCalorieElf(list []Elf) Elf {
 	}
 
 	return mostCalorieElf
+}
+
+func findTopElves(list []Elf, count int) []Elf {
+	//make a copy of original list
+	var elfListCopy = list
+
+	//create new list to hold top elves
+	var topElves []Elf
+
+	//iterate through as many times specified
+	for i := 0; i < count; i++ {
+		//find the highest calorie elf of list
+		var highestCalorieElf = findMostCalorieElf(elfListCopy)
+
+		//add elf to the topElves slice
+		topElves = append(topElves, highestCalorieElf)
+
+		//find elf index
+		var index = findElfIndex(elfListCopy, highestCalorieElf)
+
+		//remove elf from copy of elf list to iterate through again finding new highest value
+		elfListCopy = append(elfListCopy[:index], elfListCopy[index+1:]...)
+	}
+	return topElves
+}
+
+func calculateTotalTopElves(list []Elf) {
+	var totalSum int32
+	fmt.Println("Highest Calorie Elves Are :")
+
+	for i, v := range list {
+		totalSum = totalSum + v.calorieCount
+		fmt.Println("In spot: ", i+1, " is elf ID: ", v.id, "with a total of: ", v.calorieCount, "calories!")
+	}
+
+	fmt.Println("For a combined total of: ", totalSum)
 }
