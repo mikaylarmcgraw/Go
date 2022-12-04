3551ce9965b04d052876fbf52ad806959cf57f6d
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-12-04 14:01:51 -0500

    Adding answer to part 2

--- AdventOfCode2022/Day2/main.go
+++ AdventOfCode2022/Day2/main.go
@@ -15,21 +15,33 @@ type Round struct {
 }
 
 func main() {
+	fmt.Println("Which way do you want to play? Please enter 1 or 2: ")
+	var input int
+	fmt.Scanln(&input)
+
 	//create list of rounds
 	var roundList []Round
 	//retrieve input and parse into Round structures list with appropriate player selections
-	roundList = readInputValues()
+	roundList = readInputValues(input)
+
+	if input == 2 {
+		roundList = calculateChoicePoints(roundList, 2)
+		roundList = assignMyChoice(roundList) // if playing part 2 way assign my choice option first by looking at the outcome before calculating choice points
+	}
+
 	//calculate points based on your player selection
-	roundList = calculateChoicePoints(roundList)
-	//calculate outcome of game and adjust points accordingly
+	roundList = calculateChoicePoints(roundList, 1)
+	//calculate outcome of game
 	roundList = roundOutcomeCalculator(roundList)
 
-	for i := range roundList {
-		fmt.Println("Results: ", roundList[i].outcome)
-	}
+	//calculate total points
+	var pointTotal = calculatePoints(roundList)
+	//print output
+	fmt.Println("Total points: ", pointTotal)
+
 }
 
-func readInputValues() []Round {
+func readInputValues(style int) []Round {
 	var list []Round
 
 	//error handling on opening file
@@ -43,14 +55,17 @@ func readInputValues() []Round {
 
 	//scanning file for next line
 	for scanner.Scan() {
+		round := Round{}
 		//get line of file and save into string
 		input := scanner.Text()
 		//split values of string by space and hold values in array
 		inputArray := strings.Split(input, " ")
-		//obtain individual values from array
+		//obtain opponents choice and other property either out choice or outcome
 		v1 := inputArray[0]
 		v2 := inputArray[1]
-		round := Round{v1, v2, "", 0}
+		//save values for reusability sake
+		round = Round{v1, v2, "", 0}
+		//append list with new object
 		list = append(list, round)
 	}
 
@@ -62,22 +77,84 @@ func readInputValues() []Round {
 	return list
 }
 
-func calculateChoicePoints(list []Round) []Round {
+func calculateChoicePoints(list []Round, input int) []Round {
 
 	for i, v := range list {
 		switch v.myChoice {
 		// if value is rock add 1 point
 		case "X":
-			list[i].points = 1
+
+			if input == 1 {
+				list[i].points = 1
+			}
+			if input == 2 {
+				list[i].outcome = "lose" // if value is 'X' and style 2 game the outcome is lose
+			}
+
 		//if value is paper add 2 points
 		case "Y":
-			list[i].points = 2
+
+			if input == 1 {
+				list[i].points = 2
+			}
+			if input == 2 {
+				list[i].outcome = "tie" // if value is 'Y' and style 2 game the outcome is tie
+			}
+
 		//if value is scissors add 3 points
 		case "Z":
-			list[i].points = 3
+
+			if input == 1 {
+				list[i].points = 3
+			}
+
+			if input == 2 {
+				list[i].outcome = "win" // if value is 'Z' and style 2 game the outcome is win
+			}
 		}
 	}
 
+	return list
+}
+
+func assignMyChoice(list []Round) []Round {
+
+	for i, v := range list {
+
+		switch list[i].opponentChoice {
+
+		case "A":
+			if v.outcome == "win" {
+				list[i].myChoice = "Y"
+			}
+			if v.outcome == "lose" {
+				list[i].myChoice = "Z"
+			}
+			if v.outcome == "tie" {
+				list[i].myChoice = "X"
+			}
+		case "B":
+			if v.outcome == "win" {
+				list[i].myChoice = "Z"
+			}
+			if v.outcome == "lose" {
+				list[i].myChoice = "X"
+			}
+			if v.outcome == "tie" {
+				list[i].myChoice = "Y"
+			}
+		case "C":
+			if v.outcome == "win" {
+				list[i].myChoice = "X"
+			}
+			if v.outcome == "lose" {
+				list[i].myChoice = "Y"
+			}
+			if v.outcome == "tie" {
+				list[i].myChoice = "Z"
+			}
+		}
+	}
 	return list
 }
 
@@ -125,4 +202,22 @@ func roundOutcomeCalculator(list []Round) []Round {
 		}
 	}
 	return list
+}
+
+func calculatePoints(list []Round) int32 {
+	var pointSum int32
+
+	for i := range list {
+		switch list[i].outcome {
+		case "tie":
+			list[i].points = list[i].points + 3
+		case "win":
+			list[i].points = list[i].points + 6
+		case "lose":
+			list[i].points = list[i].points + 0
+		}
+
+		pointSum = pointSum + list[i].points
+	}
+	return pointSum
 }
